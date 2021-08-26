ffc37a08e3507c37152cdd070a28464a88198754
furiospro86 <furiospro86@gmail.com>
2021-08-27 02:20:19 +1000

    well done

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$url = 'https://superposuda.retailcrm.ru/';
+
+
+$data = http_build_query([
+	'order' => json_encode([
+		'site' => 'test',
+		'orderMethod' => 'test',
+		'orderType' => 'fizik',
+		'number' => '11021986',
+		'lastName' => 'Жуков',
+		'firstName' => 'Юрий',
+		'patronymic' => 'Викторович',
+		'customFields' => [
+			'prim' => 'тестовое задание'
+		],
+		'customerComment' => '',
+		'items' =>[
+			[
+				'productName' => 'Маникюрный набор AZ105R Azalita'
+			]
+		],
+		'status' => 'trouble',
+	]),
+	'apiKye' => 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb',
+]);
+$headers = [
+	'http' => [
+		'method' => 'POST',
+		'header' => 'application/x-www-form-urlencoded',
+		'X-API-KEY' => 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb',
+		'content' => $data
+	]
+];
+
+
+$context = stream_context_create($headers);
+
+$file = fopen($url.'api/v5/orders/create','r',false,$context);
+fpassthru($file);
+fclose($file);
+
+
+
