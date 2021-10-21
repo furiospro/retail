c0c1e90bd4c0aa84f40992b1cb8e623675e03581
furiospro <78708684+furiospro@users.noreply.github.com>
2021-10-21 22:33:37 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,46 @@
-<?php
-
-$url = 'https://superposuda.retailcrm.ru/';
-
-
+function debug($arr){
+    echo '<pre>'.print_r($arr,1).'</pre>';
+}
 $data = http_build_query([
-	'site' => 'test',
-	'order' => json_encode([
-		'orderMethod' => 'test',
-		'orderType' => 'fizik',
-		'number' => '11021986',
-		'lastName' => 'Жуков',
-		'firstName' => 'Юрий',
-		'patronymic' => 'Викторович',
-		'customFields' => [
-			'prim' => 'тестовое задание'
-		],
-		'customerComment' => 'https://github.com/furiospro/retail',
-		'items' =>[
-			[
-				'productName' => 'Маникюрный набор AZ105R Azalita'
-			]
-		],
-		'status' => 'trouble',
-	]),
+    'order' => json_encode([
+        'orderMethod' => 'test',
+        'orderType' => 'fizik',
+        'number' => '11021986',
+        'lastName' => 'Жуков',
+        'firstName' => 'Юрий',
+        'patronymic' => 'Викторович',
+        'customFields' => [
+        'prim' => 'тестовое задание'
+    ],
+    'customerComment' => 'https://github.com/furiospro/retail',
+    'items' =>[
+        [
+            'productName' => 'Маникюрный набор AZ105R Azalita'
+        ]
+    ],
+    'status' => 'trouble',
+    ]),
 ]);
+
 $headers = [
-	'http' => [
-		'method' => 'POST',
-		'header' => 'Content-type: application/x-www-form-urlencoded',
-		'X-API-KEY' => 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb',
-		'content' => $data
-	]
+    'Content-type:application/x-www-form-urlencoded',
+    'X-API-KEY:QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb',
 ];
 
+$url = 'https://superposuda.retailcrm.ru/api/v5/orders/create';
 
-$context = stream_context_create($headers);
+$curl =curl_init($url);
+curl_setopt($curl,CURLOPT_URL,$url);
+curl_setopt($curl, CURLOPT_HEADER, true);
+curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl,CURLOPT_POST,true);
+curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
 
-$file = fopen($url.'api/v5/orders/create','r',false,$context);
-fpassthru($file);
-fclose($file);
+$head = curl_exec($curl);
+preg_match("/\{(.*)\}/si",$head,$match);
+curl_close($curl);
 
-
+debug(json_decode($match[0]));
 
